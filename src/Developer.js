c858faeaa81023911c4fcedd77802e74ebb3e65e
Bharat Rele <rele.bharat@gmail.com>
2017-10-29 08:20:55 -0700

    creating components

---
+++ src/Developer.js
@@ -0,0 +1,13 @@
+class Developer{
+    constructor(firstname, lastname){
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    getName(){
+        return this.firstname + ' ' + this.lastname;
+    }
+}
+
+const bharat = new Developer('Bharat', 'Rele');
+console.log(bharat.getName());
